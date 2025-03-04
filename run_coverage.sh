e22349ab79fbb7f4f98568b41a27dfef2795d971
Sergey Rybakov <srybakov@openweathermap.org>
2025-03-04 21:51:43 +0200

    workflow

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,2 +1,4 @@
-pytest --cov=./call_gate --cov-report=html --cov-report=term-missing
+docker compose down
+docker compose up -d
+pytest --cov=./call_gate --cov-report=html --cov-report=term-missing --cov-branch
 echo 'Find html report at ./tests/code_coverage/index.html'
