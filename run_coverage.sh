9e805d37389afe51d2815933b6a752c478fcd41d
Sergey Rybakov <srybakov@openweathermap.org>
2025-03-05 13:50:46 +0200

    Makefile

--- run_coverage.sh
+++
@@ -1,4 +0,0 @@
-docker compose down
-docker compose up -d
-pytest --cov=./call_gate --cov-report=html --cov-report=term-missing --cov-branch
-echo 'Find html report at ./tests/code_coverage/index.html'
